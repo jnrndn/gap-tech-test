85d98b951730a1b2c07e4c274892b3b4d2d2d48a
Juan Jose Rendon <juan.rendon@yuxiglobal.com>
2019-02-17 22:32:04 -0500

    fix: lint error

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatInputModule, MatMenuModule, MatSelectModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatInputModule,
+  MatMenuModule,
+  MatSelectModule
+} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
